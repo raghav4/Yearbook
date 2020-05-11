abae3ae63622c0b9da078c641cc1d0a2ae8c2f84
Restyled.io <commits@restyled.io>
2020-05-11 11:18:12 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
--- src/utils/schemas.js
+++ src/utils/schemas.js
@@ -2,63 +2,55 @@ import Joi from 'joi-browser';
 
 const LoginSchema = () => {
   return {
-    email: Joi.string()
-      .email()
-      .error(() => {
-        return {
-          message: 'Please enter a valid Email ID',
-        };
-      })
-      .required(),
-    password: Joi.string()
-      .error(() => {
-        return {
-          message: 'Password cannot be empty',
-        };
-      })
-      .required(),
+    email : Joi.string()
+                .email()
+                .error(() => {
+                  return {
+                    message : 'Please enter a valid Email ID',
+                  };
+                })
+                .required(),
+    password : Joi.string()
+                   .error(() => {
+                     return {
+                       message : 'Password cannot be empty',
+                     };
+                   })
+                   .required(),
   };
 };
 
 const DetailsSchema = () => {
   return {
-    contactEmail: Joi.string().email().label('Contact Email'),
-    facebook: Joi.string()
-      .uri()
-      .label('Facebook')
-      .error(() => {
-        return {
-          message: 'Please enter a valid Facebook profile URL',
-        };
-      }),
-    linkedin: Joi.string()
-      .uri()
-      .label('Linkedin')
-      .error(() => {
-        return {
-          message: 'Please enter a valid LinkedIn profile URL',
-        };
-      }),
-    whatsappNo: Joi.string()
-      .length(10)
-      .regex(/^\d+$/)
-      .label('WhatsApp Number')
-      .error(() => {
-        return {
-          message: 'Input should be a number of length 10',
-        };
-      }),
-    contactNo: Joi.string()
-      .label('Contact Number')
-      .regex(/^\d+$/)
-      .error(() => {
-        return {
-          message: 'Input should be a number of length 10',
-        };
-      }),
-    snapchat: Joi.string().label('Snapchat'),
-    instagram: Joi.string().label('Instagram'),
+    contactEmail : Joi.string().email().label('Contact Email'),
+    facebook : Joi.string().uri().label('Facebook').error(() => {
+      return {
+        message : 'Please enter a valid Facebook profile URL',
+      };
+    }),
+    linkedin : Joi.string().uri().label('Linkedin').error(() => {
+      return {
+        message : 'Please enter a valid LinkedIn profile URL',
+      };
+    }),
+    whatsappNo : Joi.string()
+                     .length(10)
+                     .regex(/^\d+$/)
+                     .label('WhatsApp Number')
+                     .error(() => {
+                       return {
+                         message : 'Input should be a number of length 10',
+                       };
+                     }),
+    contactNo :
+        Joi.string().label('Contact Number').regex(/^\d+$/).error(() => {
+          return {
+            message : 'Input should be a number of length 10',
+          };
+        }),
+    snapchat : Joi.string().label('Snapchat'),
+    instagram : Joi.string().label('Instagram'),
   };
 };
 
-export { LoginSchema, DetailsSchema };
+export {LoginSchema, DetailsSchema};
